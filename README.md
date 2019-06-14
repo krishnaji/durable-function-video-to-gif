3de9c0a7f89b6faa15e150f0e30e089dc08487c3
Shrikrishna Khose <shkhose@microsoft.com>
2019-06-14 18:56:51 -0400

    Adjusted framerate and size

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # [Azure Durable Functions](https://docs.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-overview) : Video to GIF conversion
+![](demo.gif)
+
 This application uses Azure Durable functions, Blob Storage, Event Grid and Application Insights.
 
 ## Provision Functions App and other resources
--- Task_createGif/index.js
+++ Task_createGif/index.js
@@ -4,7 +4,7 @@ const spawn = require('child_process').spawn;
 module.exports =  function (context) {
       context.log('@@@ Activity 3 @@@'); 
       const gifName = (context.bindings.name.split('.'))[0]+'.gif';
-      const ffmpeg=   spawn(ffmpegPath, ['-i',context.bindings.name,'-f','gif',gifName]);   
+      const ffmpeg=   spawn(ffmpegPath, ['-i',context.bindings.name,'-vf','fps=12,scale=854:-1','-f','gif',gifName]);   
       ffmpeg.on('exit',()=>{
         context.done(null, {fileName:gifName});
       });
